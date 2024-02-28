b0a1179d8ed31f5888cb00672c33a457f8733d5c
JouCarrasco <joucarrasco91@gmail.com>
2024-02-28 08:36:35 -0600

    segunda actualizacion

--- src/Pages/Solicitud.jsx
+++ src/Pages/Solicitud.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import '../Pages/Styles.css'
+import React, { useState } from "react";
+import NavbarX from "../Atoms/Navbar";
+import reclu from "../assets/reclu.jpg";
+import image7 from "../assets/image7.jpg";
+import "../Pages/Styles.css";
 
 function Solicitud() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [institution, setInstitution] = useState('');
-  const [skills, setSkills] = useState('');
-  const [coverLetter, setCoverLetter] = useState('');
-  const [cv, setCv] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [institution, setInstitution] = useState("");
+  const [skills, setSkills] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
+  const [cv, setCv] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,65 +19,99 @@ function Solicitud() {
 
   return (
     <div>
-      <h1 className='soliem'>Solicitud de empleo</h1>
-      <form onSubmit={handleSubmit} className='card-form'>
-        <label htmlFor="name">Nombre completo</label>
+      <NavbarX />
+      <img src={reclu} className="reclu-img"/>
+      <div className="info-1">
+        <h2 className="h2">
+          ¡Únete a nuestro equipo y construyamos juntos el futuro!
+        </h2>
+        <p className="h3">
+          ¿Eres un apasionado de tu trabajo y buscas un lugar donde crecer
+          profesionalmente?
+        </p>
+        <p className="p">
+          En Eolic Power, creemos que el talento es la clave del éxito. <br />
+          Por eso, buscamos personas como tú, con talento, compromiso <br />y
+          ganas de formar parte de una empresa en constante crecimiento.
+        </p>
+        <img src={image7} className="img7"/>
+      </div>
+
+      <form onSubmit={handleSubmit} className="card-form">
+        <label htmlFor="name" className="la-nom">
+          Nombre completo
+        </label>
         <input
+          className="input-nom"
           type="text"
           id="name"
           name="name"
           value={name}
           onChange={(event) => setName(event.target.value)}
         />
-        <label htmlFor="email">Correo electrónico</label>
+        <label htmlFor="email" className="la-ema">
+          Correo electrónico
+        </label>
         <input
+          className="in-email"
           type="email"
           id="email"
           name="email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
-        <label htmlFor="phone">Número de teléfono</label>
+        <label htmlFor="phone" className="la-number">
+          Número de teléfono
+        </label>
         <input
+          className="in-number"
           type="tel"
           id="phone"
           name="phone"
           value={phone}
           onChange={(event) => setPhone(event.target.value)}
         />
-        <label htmlFor="institution">Institución académica</label>
+        <label htmlFor="institution" className="la-academy">
+          Institución académica
+        </label>
         <input
+          className="in-academy"
           type="text"
           id="institution"
           name="institution"
           value={institution}
           onChange={(event) => setInstitution(event.target.value)}
         />
-        <label htmlFor="skills">Habilidades técnicas</label>
+        <label htmlFor="skills" className="la-skills">Habilidades técnicas</label>
         <textarea
+        className="tex-skills"
           id="skills"
           name="skills"
           value={skills}
           onChange={(event) => setSkills(event.target.value)}
         />
-        <label htmlFor="coverLetter">Carta de presentación</label>
+        <label htmlFor="coverLetter" className="la-cart">Carta de presentación</label>
         <input
+        className="in-cart"
           type="file"
           id="coverLetter"
           name="coverLetter"
           onChange={(event) => setCoverLetter(event.target.files[0])}
         />
-        <label htmlFor="cv">Curriculum vitae (CV)</label>
+        <label htmlFor="cv" className="la-cv">Curriculum vitae (CV)</label>
         <input
+        className="in-cv"
           type="file"
           id="cv"
           name="cv"
           onChange={(event) => setCv(event.target.files[0])}
         />
-        <button type="submit">Enviar solicitud</button>
+        <button type="submit" className="send">
+          Enviar solicitud
+        </button>
       </form>
     </div>
   );
-};
+}
 
 export default Solicitud;
